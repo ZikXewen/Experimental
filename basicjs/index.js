7a2c7491e81a61929f4ede7981207caffa4fdf16
ZikLen <datasup12@gmail.com>
2020-06-20 03:05:00 +0700

    190620

---
+++ basicjs/index.js
@@ -0,0 +1,15 @@
+//console.log('Hello World');
+let name = 'Boi';
+let person = {
+    name: 'Gril',
+    age: 14
+};
+function greet(inp){
+    console.log('Hello ' + inp);
+}
+function square(inp){
+    return inp * inp;
+}
+//console.log(person.name);
+greet(person.name);
+console.log(square(6));
