c8d087de10e53912f2d50fecf2cbb27b1844045b
Tat Srd <homeysa@hotmail.com>
2021-03-25 13:46:42 +0700

    new file:   basicreact/

---
+++ basicreact/myapp/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 3 },
+      { id: 2, value: 1 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  constructor() {
+    super();
+    console.log("App - Constructor");
+  }
+  componentDidMount() {
+    console.log("App - Mounted");
+  }
+  componentDidUpdate() {
+    console.log("App - Updated");
+  }
+  componentWillUnmount() {
+    console.log("App - Will Unmount");
+  }
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  render() {
+    console.log("App - Rendered");
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+export default App;
---
+++ basicreact/myapp/src/components/counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+
+  render() {
+    const { counter, onIncrement, onDelete } = this.props;
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => onIncrement(counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => onDelete(counter.id)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
